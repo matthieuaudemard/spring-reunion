55ee39c530c3028ecc56a1178dbb5e816ad75911
Matthieu Audemard <matthieu.audemard@protonmail.com>
2019-05-20 01:21:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cgi010-reunion
---
+++ src/test/java/com/udev/reunion/ReunionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udev.reunion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReunionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
