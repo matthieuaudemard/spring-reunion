49c68360027868d6dbc9039eee937e34867a709a
Matthieu Audemard <matthieu.audemard@protonmail.com>
2019-05-20 15:46:08 +0200

    L'ajout de commentaire est désormais possible.

--- src/main/java/com/udev/reunion/controller/CommentController.java
+++ src/main/java/com/udev/reunion/controller/CommentController.java
@@ -2,18 +2,21 @@ package com.udev.reunion.controller;
 
 
 import com.udev.reunion.domain.Comment;
+import com.udev.reunion.domain.Message;
+import com.udev.reunion.domain.User;
 import com.udev.reunion.model.CommentJson;
 import com.udev.reunion.service.CommentService;
+import com.udev.reunion.service.MessageService;
+import com.udev.reunion.service.UserService;
 import com.udev.reunion.util.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Date;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -23,10 +26,14 @@ import static java.util.stream.Collectors.toList;
 public class CommentController {
 
     private CommentService commentService;
+    private UserService userService;
+    private MessageService messageService;
 
     @Autowired
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, UserService userService, MessageService messageService) {
         this.commentService = commentService;
+        this.userService = userService;
+        this.messageService = messageService;
     }
 
     @GetMapping(value = "/comments/{messageId}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -39,5 +46,28 @@ public class CommentController {
         return messages.stream()
                 .map(Mapper::convert)
                 .collect(toList());
+    }
+
+    @PostMapping(value = "/comment/add")
+    public RedirectView commentSubmit(@ModelAttribute CommentJson commentJson) {
+        User user = userService.findById(commentJson.getSenderId());
+        Message message = messageService.getMessageById(commentJson.getMessageId());
+
+        if (user != null && message != null) {
+            Comment comment = new Comment();
+            comment.setMessage(message);
+            comment.setSender(user);
+            comment.setBody(commentJson.getCommentBody());
+            commentService.send(comment);
+            RedirectView rv = new RedirectView();
+            rv.setContextRelative(true);
+            rv.setUrl("/message/" + message.getId());
+            return rv;
+        }
+
+        else {
+            return new RedirectView("/messageCreateError");
+        }
+
     }
 }
